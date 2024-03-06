f5f2424b5be10b993c3bc1ef7d5c53406ff882e0
Hanzla Sadaqat <hanzlasadaqatrajput@gmail.com>
2024-03-07 03:38:45 +0500

    deploy

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const cors = require("cors");
-const path = require("path");
 const dotenv = require("dotenv");
 const router = require("./routes");
 const connectDB = require("./database");
@@ -12,7 +11,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json({ limit: "100mb" }));
-app.use(express.urlencoded({ extended: true }));
+// app.use(express.urlencoded({ extended: true }));
 app.use("/api", router);
 // app.use("/*", (req, res) => res.send("404 Error in the routes"));
 
